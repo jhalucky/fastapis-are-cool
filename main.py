d1b65eaa28bf09e41eee159a766afe8fcb4960b3
Lucky Jha <jhalucky61@gmail.com>
2026-09-16 19:08:04 +0530

    started out fastapi

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {'message': "hello world!"}
+
+
+@app.get("/about")
+def about():
+    return {"about": "Just building around, engineering everywhere"}
+
+
+@app.post("/registration-form")
+def registration():
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+    return name, age
