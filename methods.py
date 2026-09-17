23c0f5c2f9c98f8f9ec6d9cb3821bbc342017e45
Lucky Jha <jhalucky61@gmail.com>
2026-09-17 10:24:08 +0530

    query params

--- methods.py
+++ methods.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -34,3 +34,22 @@ def view_student(student_id: str = Path(..., description="ID of the student in t
         return data[student_id]
 
     raise HTTPException(status_code=404, detail='patient not found')
+
+
+@app.get('/sort')
+def sort_students(sort_by: str = Query(..., description='Sort on the basis of semester'), order: str = Query('asc',desc='sort in asc or desc order')):
+
+    valid_fields = 'semester'
+
+    if sort_by is valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+
+
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between in asc and desc')
+
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key= lambda x:x.get(sort_by, 0), reverse=sort_order)
