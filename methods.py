3124a6b86703dfb7e763705b6d90d1eb1e957e5d
Lucky Jha <jhalucky61@gmail.com>
2026-09-20 18:54:20 +0530

    CRUD completed

--- methods.py
+++ methods.py
@@ -161,3 +161,18 @@ def update_student(student_id: str, student_update: StudentUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={"message": "Patient updated" })
+
+
+@app.delete('/delete/{student_id}')
+def  delete_student(stduent_id: str):
+
+    data = load_data()
+
+    if stduent_id not in data:
+        raise HTTPException(status_code=404, detail='Student not found')
+
+    del data[stduent_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message": "Student deleted."})
